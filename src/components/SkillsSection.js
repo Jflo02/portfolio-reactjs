b9b47b312dd2c6b8aa2bbc07b10ef859187acc79
flori <florimond2002@gmail.com>
2021-11-19 23:08:03 +0100

    ajout section skills dans la home page

---
+++ src/components/SkillsSection.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import styled from 'styled-components';
+import SectionTitle from './SectionTitle';
+import PText from './PText';
+import Button from './Button';
+
+const SkillsSectionStyle = styled.div`
+  padding: 10rem 0;
+
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    text-align: left;
+  }
+
+  .skillsSection__left {
+    flex: 1;
+  }
+
+  .section-title {
+    text-align: left;
+  }
+
+  .para {
+    margin-top: 2rem;
+    margin-left: 0;
+  }
+
+  .skillsSection__buttons {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 2rem;
+    margin-top: 2rem;
+  }
+  @media only screen and (max-width: 950px) {
+    .aboutSection__left {
+      flex: 4;
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .container {
+      flex-direction: column;
+      text-align: center;
+    }
+    .skillsSection__left {
+      width: 100%;
+    }
+    .section-tile: {
+      text-align: center;
+    }
+    .para {
+      margin: 0 auto;
+      margin-top: 2rem;
+    }
+    .skillsSection__buttons {
+      flex-direction: column;
+      gap: 0;
+      .button-wrapper,
+      a {
+        width: 100%;
+        text-align: center;
+      }
+    }
+  }
+`;
+
+function SkillsSection() {
+  return (
+    <SkillsSectionStyle>
+      <div className="container">
+        <div className="skillsSection__left">
+          <SectionTitle
+            subheading="Let me show you my competences"
+            heading="About my skills"
+          />
+          <PText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            faucibus dictum elit, ut tristique dui lobortis nec. Morbi feugiat,
+            erat quis consequat dapibus, ex lorem sollicitudin metus, sit amet
+          </PText>
+          <div className="skillsSection__buttons">
+            <Button btnLink="/skills" btnText="competences" outline />
+          </div>
+        </div>
+      </div>
+    </SkillsSectionStyle>
+  );
+}
+
+export default SkillsSection;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
+import SkillsSection from '../components/SkillsSection';
 
 function Home() {
   return (
     <div>
       <HeroSection />
+      <SkillsSection />
     </div>
   );
 }
