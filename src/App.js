30631df3c428a1742a8b974373a614da0fe1f0dd
flori <florimond2002@gmail.com>
2021-11-22 08:39:15 +0100

    ajout d'une section projet dans la home page

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavMenu from './components/NavMenu';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills';
+import Contact from './pages/Contact';
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <NavMenu />
+
+        <Switch>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/skills">
+            <Skills />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
